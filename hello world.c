3ceff2477899e655f4dc9fc70ac5cadfca5d8265
maymanikant2008-creator <maymanikant2008@gmail.com>
2026-08-10 17:34:55 +0530

    Create hello world.c
    program to take input n from user then use while and print hello world n times

---
+++ hello world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    printf("entre the value of n");
+    scanf("%d",&n);
+    int i;
+    while (i<=n) {
+    	printf("hello world \n");
+    	i++;
+	}
+     
+    return 0;
+}
